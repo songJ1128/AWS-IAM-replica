8633c162453ce84ad69174b4f15cd171b074661b
songJ1128 <jerrysong1128@gmail.com>
2024-10-25 11:43:16 -0400

    finished base of policy

--- Policy.java
+++ Policy.java
@@ -18,3 +18,66 @@ public class Policy {
         this.conditions = new HashMap<>();
         this.parentPolicy = null;
     }
+    public String getName() {
+        return name;
+    }
+
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<String> getResources() {
+        return resources;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void addCondition(String key, Condition condition) {
+        conditions.put(key, condition);
+    }
+
+    public boolean evaluateConditions(Map<String, String> context) {
+        for (Condition condition : conditions.values()) {
+            if (!condition.evaluate(context)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public void setParentPolicy(Policy parentPolicy) {
+        this.parentPolicy = parentPolicy;
+    }
+
+
+    public boolean isActionAllowed(Permission action, String resource, Map<String, String> context) {
+        if (permissions.contains(action) && resources.contains(resource) && evaluateConditions(context)) {
+            return true;
+        }
+        if (parentPolicy != null) {
+            return parentPolicy.isActionAllowed(action, resource, context);
+        }
+
+        return false;
+    }
+    public void updateVersion(String newVersion) {
+        this.version = newVersion;
+    }
+
+    @Override
+    public String toString() {
+        return "Policy{" +
+                "name='" + name + '\'' +
+                ", permissions=" + permissions +
+                ", resources=" + resources +
+                ", version='" + version + '\'' +
+                ", conditions=" + conditions +
+                ", parentPolicy=" + (parentPolicy != null ? parentPolicy.getName() : "none") +
+                '}';
+    }
+}
+
+
